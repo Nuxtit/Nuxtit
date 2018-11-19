81c9546d6fec5c0d505d1757337195b07eca84e4
redusa <--global>
2018-11-19 01:55:31 -0500

    add clear-ls

---
+++ pages/clear-ls.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  section.container
+    | Clearing...
+</template>
+
+<script>
+import sleep from '~/lib/sleep';
+
+export default {
+  // middleware: ['auth'],
+  async mounted() {
+    window.localStorage.clear();
+    await sleep(2000);
+    window.location = '/';
+  },
+};
+</script>
+
+<style>
+</style>
