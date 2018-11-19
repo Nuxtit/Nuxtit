6467e0ff06bcf91b5f5d6324444bbf2e10dd573c
anon <>
2018-11-19 18:55:24 -0500

    update PostEntry css, add click-to-expand thumbnails, support for more types of thumbnails

--- components/PostThumbnail.vue
+++ components/PostThumbnail.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .posts-thumbnail
-    span(v-if="post.data.thumbnail === 'image'")
+    span(v-if="!imageSrc && post.data.thumbnail === 'image'")
       | image
     span(v-else-if="post.data.thumbnail === 'default'" title="default")
       i.fa.fa-fw.fa-5x.fa-question-circle-o
@@ -8,17 +8,24 @@
       i.fa.fa-fw.fa-5x.fa-file-text
     span(v-else-if="post.data.thumbnail === 'spoiler'" title="spoiler")
       i.fa.fa-fw.fa-5x.fa-question-circle-o
-    span(v-else-if="post.data.thumbnail === 'nsfw'" title="nsfw")
+    span.cursor-pointer(
+      v-else-if="post.data.thumbnail === 'nsfw'" title="nsfw"
+      @click.prevent.default='$emit("expand")'
+    )
       i.fa.fa-fw.fa-5x.fa-user-secret
-    img(
-      v-else-if="post.data.thumbnail"
-      :src="post.data.thumbnail"
+    img.cursor-pointer(
+      v-else-if="imageSrc"
+      :src="imageSrc"
       :alt="post.data.title"
+      @click.prevent.default='$emit("expand")'
     )
     span(v-else) NO_THUMB
 </template>
 
 <script>
+import get from 'lodash/get';
+import find from 'lodash/find';
+
 export default {
   name: 'PostThumbnail',
   props: {
@@ -27,8 +34,36 @@ export default {
       required: true,
     },
   },
+  computed: {
+    imageSrc() {
+      const { thumbnail, preview } = this.post.data;
+      if (thumbnail) {
+        if (thumbnail.startsWith('https://')) {
+          return thumbnail;
+        }
+      }
+      if (preview && preview.images && preview.images.length) {
+        for (let i = 0, p; i < preview.images.length; i++) {
+          p = find(get(preview.images[i], 'resolutions'), p => {
+            return p && p.url;
+          });
+          if (p) {
+            return p.url;
+          }
+          const p = find(get(preview.images[i], 'source'), p => {
+            return p && p.url;
+          });
+          if (p) {
+            return p.url;
+          }
+        }
+      }
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="sass">
+.cursor-pointer
+  cursor: pointer;
 </style>
