5e1261a13a27662f838e1c2f73f98561405d689b
anon <>
2018-11-23 00:39:12 -0500

    updates

--- components/PostThumbnail.vue
+++ components/PostThumbnail.vue
@@ -2,7 +2,7 @@
   .posts-thumbnail
     span(v-if="!imageSrc && post.data.thumbnail === 'image'")
       | image
-    span(v-else-if="post.data.thumbnail === 'default'" title="default")
+    span(v-else-if="!imageSrc && post.data.thumbnail === 'default'" title="default")
       i.fa.fa-fw.fa-5x.fa-question-circle-o
     span(v-else-if="post.data.thumbnail === 'self'" title="self")
       i.fa.fa-fw.fa-5x.fa-file-text
@@ -38,21 +38,33 @@ export default {
     // @todo is this a good idea?
     // 'preview.images.0.variants.mp4.source.url',
     'preview.images.0.variants.gif.source.url',
-    'preview.images.0.source.url',
     'preview.images.0.resolutions.0.url',
-    'url',
+    'preview.images.0.source.url',
   ],
   computed: {
     imageSrc() {
       let src = this.post.data.thumbnail;
+      let path;
       if (src && src.startsWith('https://')) {
         return src;
       }
       for (let i = 0, len = this.$options.srcPaths.length; i < len; i++) {
-        src = get(this.post.data, this.$options.srcPaths[i]);
+        path = this.$options.srcPaths[i];
+        src = get(this.post.data, path);
         if (src && src.startsWith('https://')) {
           return src;
         }
+      }
+      for (let i = 0, len = this.$options.srcPaths.length; i < len; i++) {
+        path = 'crosspost_parent_list.0.' + this.$options.srcPaths[i];
+        src = get(this.post.data, path);
+        if (src && src.startsWith('https://')) {
+          return src;
+        }
+      }
+      src = this.post.data.url;
+      if (src && src.startsWith('https://')) {
+        return src;
       }
       return null;
     },
--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Redusa',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
