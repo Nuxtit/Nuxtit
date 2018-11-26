610cef559bd00e910ff5b7158181ef99005e79b1
anon <>
2018-11-25 22:59:25 -0500

    view your own multireddits

---
+++ components/MultiList.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .multis-list
+    MultiEntry(
+      v-for='multi in multis.data.children'
+      :multi='multi'
+      :key='multi.data.name'
+    )
+</template>
+
+<script>
+import MultiEntry from '~/components/MultiEntry';
+
+export default {
+  name: 'MultiList',
+  components: {
+    MultiEntry,
+  },
+  props: {
+    multis: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
