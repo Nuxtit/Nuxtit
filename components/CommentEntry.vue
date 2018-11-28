8bc268dd3068af1694eab3c15f0253e5e0b9f909
redusa <--global>
2018-11-27 21:45:33 -0500

    bugfix

--- components/CommentEntry.vue
+++ components/CommentEntry.vue
@@ -186,7 +186,7 @@ export default {
     parentTo() {
       const { parent_id, permalink, link_id, id } = this.comment.data;
       if (parent_id && parent_id !== link_id && permalink) {
-        return '/' + permalink.replace(id, parent_id.slice(3));
+        return permalink.replace(id, parent_id.slice(3));
       }
       return null;
     },
