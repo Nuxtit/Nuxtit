4ee200be23ebaf892dcdcff8f9fb22cb0ea42c7c
redusa <--global>
2018-11-28 19:35:39 -0500

    added submit text

--- pages/r/_subreddit/submit.vue
+++ pages/r/_subreddit/submit.vue
@@ -1,13 +1,22 @@
 <template lang="pug">
-  PostForm(
-    :subreddit='subreddit'
-  )
+  div
+    br
+    .container
+      .alert.alert-primary
+        ItemHtml(:value='subreddit.data.submit_text_html')
+    br
+    .container
+      PostForm(
+        :subreddit='subreddit'
+      )
 </template>
 <script>
+import ItemHtml from '~/components/ItemHtml';
 import PostForm from '~/components/PostForm';
 export default {
   middleware: ['auth'],
   components: {
+    ItemHtml,
     PostForm,
   },
   props: {
