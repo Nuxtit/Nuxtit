a708a74f57cf3ae73fb8f3e9bb8a77f6fcdb861c
redusa <--global>
2018-12-13 22:46:43 -0500

    tags for known bots

---
+++ components/BotBadge.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  b-badge(
+    variant='dark'
+  )
+    i.fa.fa-fw.fa-android
+    | &#32;bot
+</template>
+
+<script>
+export default {
+  name: 'BotBadge',
+};
+</script>
+
+<style lang="sass">
+</style>
