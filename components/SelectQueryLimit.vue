5f00d565fc6e99f59703cb651565e72d6adb0807
redusa <--global>
2019-01-21 14:43:51 -0500

    WIP pushshift

--- components/SelectQueryLimit.vue
+++ components/SelectQueryLimit.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   b-select(
     name='limit'
-    :options='$options.limitOptions'
+    :options='limitOptions'
     v-model='limit'
   )
 </template>
@@ -14,7 +14,27 @@ import QueryParamLimit from '~/mixins/QueryParamLimit';
 export default {
   name: 'SelectQueryLimit',
   mixins: [QueryParamLimit],
-  limitOptions: Object.freeze([5, 15, 25, 50, 75, 100]),
+  limitOptions: Object.freeze([
+    // must reddit api calls only return 100
+    // at least from the ones I actually use
+    5, 15, 25, 50, 75, 100, // eslint-disable-line
+    // pushshift allows more
+    250, 500, // eslint-disable-line
+  ]),
+  props: {
+    max: {
+      type: Number,
+      required: false,
+      default() {
+        return 100;
+      },
+    },
+  },
+  computed: {
+    limitOptions() {
+      return this.$options.limitOptions.filter(v => !(v > this.max));
+    },
+  },
 };
 </script>
 
