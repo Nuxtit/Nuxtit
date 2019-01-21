afadc4b32e53550d7257b9f89c3b201ead232a2f
redusa <--global>
2019-01-21 18:04:23 -0500

    wip pushshift

--- pages/pushshift/search.vue
+++ pages/pushshift/search.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   .search-page
-    .container
+    .container(
+      @keyup.enter.prevent.stop="fetchItems"
+    )
       .row
         .col
           SelectQueryKind()
@@ -19,12 +21,19 @@
           SelectQueryText(path="before" placeholder="before: 1{s,m,h,d}")
       .row
         .col
-          SearchInput(
-            :busy="fetching"
-            @update-search="fetchItems"
+          SelectQueryText(path="q" placeholder="query")
+          b-button.my-2.my-sm-0(
+            :disabled="fetching"
+            size='sm'
+            type='submit'
+            @click.prevent.stop="fetchItems"
           )
+            i.fa.fa-fw.fa-btn.fa-spinner.fa-spin(v-if="fetching")
+            i.fa.fa-fw.fa-btn.fa-search(v-if="!fetching")
+            | &#32;
+            | Search
           br
-          p
+          p.small
             strong aaaa bbbb
             br
             span find items containing "aaaa" AND "bbbb"
@@ -37,66 +46,47 @@
             br
             span find items containing "aaaa" OR "bbbb", and always "cccc"
             br
-        .col
-          p
-            strong subreddit:<em>subreddit</em>
+            strong cccc | (aaaa | bbbb) -dddd
             br
-            span find submissions in "subreddit"
+            span find items containing "aaaa" OR "bbbb", and always "cccc", and will exclude any result containing "dddd"
             br
-            strong author:<em>username</em>
+            strong cccc | (aaaa | bbbb) -dddd -eeee*
             br
-            span find submissions by "username"
+            span same as above, but will exclude matches containing "eeee" and exclude matches that contain a match for the wildcard phrase "eeee*"
             br
-            strong site:<em>example.com</em>
-            br
-            span find submissions from "example.com"
-            br
-            strong url:<em>text</em>
-            br
-            span search for "text" in url
-            br
-            strong selftext:<em>text</em>
-            br
-            span search for "text" in self post contents
-            br
-            strong self:yes (or self:no)
-            br
-            span include (or exclude) self posts
-            br
-            strong nsfw:yes (or nsfw:no)
-            br
-            span include (or exclude) results marked as NSFW
-    template(v-if='$route.query.q || !zeroResults')
-      //- RedditPagination(
-      //-   :collection='items'
-      //-   :fetching='fetching'
-      //- )
-      MixedList(:items='items')
-      //- RedditPagination(
-      //-   v-if='showBottomPagination'
-      //-   :collection='items'
-      //-   :fetching='fetching'
-      //- )
-    //- template(v-if='!$route.query.q')
-    //-   br
-    //-   .container
-    //-     .alert.alert-primary
-    //-       | Type in a query to start searching
-    template(v-if='!zeroResults')
-      .pull-right
-        nuxt-link.btn.btn-pimary(
-          v-if="lastCreatedAt"
-          :to="$mergeRouteQuery({ before: lastCreatedAt + 1 })"
-          @click.native="nextPage"
-        ) next page
-    template(v-if='$route.query.q && zeroResults')
-      br
-      .container
-        .alert.alert-info
-          | Zero Results
+    .container
+      template(v-if='$route.query.q || !zeroResults')
+        //- RedditPagination(
+        //-   :collection='items'
+        //-   :fetching='fetching'
+        //- )
+        MixedList(:items='items')
+        //- RedditPagination(
+        //-   v-if='showBottomPagination'
+        //-   :collection='items'
+        //-   :fetching='fetching'
+        //- )
+      //- template(v-if='!$route.query.q')
+      //-   br
+      //-   .container
+      //-     .alert.alert-primary
+      //-       | Type in a query to start searching
+      template(v-if='!zeroResults')
+        .pull-right
+          nuxt-link.btn.btn-pimary(
+            v-if="lastCreatedAt"
+            :to="$mergeRouteQuery({ before: lastCreatedAt + 1 })"
+            @click.native="nextPage"
+          ) next page
+      template(v-if='$route.query.q && zeroResults')
+        br
+        .container
+          .alert.alert-info
+            | Zero Results
 </template>
 
 <script>
+import bButton from 'bootstrap-vue/es/components/button/button';
 import ValidatePostSort from '~/mixins/ValidatePostSort';
 import MixedList from '~/components/MixedList.vue';
 import PushshiftList from '~/components/PushshiftList.vue';
@@ -113,6 +103,7 @@ export default {
   middleware: ['auth'],
   defaultSort: 'new',
   components: {
+    bButton,
     MixedList,
     PushshiftList,
     RedditPagination,
