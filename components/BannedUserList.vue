d2aa9d5e3b220a7a9e003dc0ce33afe69db0561a
anon <>
2019-02-23 12:30:08 -0500

    FIX moderators can now see subreddit banned users page

---
+++ components/BannedUserList.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .banned-users-list
+    BannedUserEntry(
+      v-for='user in users.data.children'
+      v-show='!user.redusaHide'
+      :user='user'
+      :key='user.name'
+    )
+</template>
+
+<script>
+import BannedUserEntry from '~/components/BannedUserEntry';
+
+export default {
+  name: 'BannedUserList',
+  components: {
+    BannedUserEntry,
+  },
+  props: {
+    users: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
