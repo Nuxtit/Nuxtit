de7f63735bd3b2814385cc1aa16c1003d551b57b
redusa <--global>
2019-02-23 00:54:41 -0500

    enable lock/nsfw/spoiler toggles for posts

--- components/LockButton.vue
+++ components/LockButton.vue
@@ -1,22 +1,21 @@
 <template lang="pug">
-  span.btn-remove(
-    v-show='false /* @todo */'
+  span.btn-lock(
     v-disabled="busy"
     :class='classes'
-    @click.stop.prevent="remove"
+    @click.stop.prevent="lock"
   )
     i.fa.fa-fw.fa-btn.fa-spinner.fa-spin(v-if='busy')
-    i.fa.fa-fw.fa-btn.fa-remove(v-else)
-    span(v-if='busy && item.data.removed') unremoving
-    span(v-else-if='busy && !item.data.removed') removing
-    span(v-else-if='item.data.removed') removed
-    span(v-else) remove
+    i.fa.fa-fw.fa-btn.fa-lock(v-else)
+    span(v-if='busy && item.data.locked') unlocking
+    span(v-else-if='busy && !item.data.locked') locking
+    span(v-else-if='item.data.locked') locked
+    span(v-else) lock
 </template>
 
 <script>
 import { startMinWait } from '~/lib/sleep';
 
-// remove is the moderator action of removing an item from public visibility
+// lock is the moderator action of locking an item from public visibility
 
 export default {
   name: 'LockButton',
@@ -35,37 +34,37 @@ export default {
   computed: {
     classes() {
       return {
-        'text-success': this.item.data.removed === true,
+        'text-success': this.item.data.locked === true,
       };
     },
   },
   methods: {
-    async remove($event) {
-      // const { removed, name } = this.item.data;
-      // const minWait = startMinWait();
-      // try {
-      //   this.busy = true;
-      //   const response = await this.$reddit.post(
-      //     `/api/${removed ? 'remove' : 'unremove'}`,
-      //     {
-      //       // category: '???',
-      //       id: name, // fullname
-      //     },
-      //   );
-      //   this.item.data.removed = !removed;
-      // } catch (err) {
-      //   console.error(err);
-      //   this.error = err;
-      // } finally {
-      //   await minWait;
-      //   this.busy = false;
-      // }
+    async lock($event) {
+      const { locked, name } = this.item.data;
+      const minWait = startMinWait();
+      try {
+        this.busy = true;
+        const response = await this.$reddit.post(
+          `/api/${locked ? 'unlock' : 'lock'}`,
+          {
+            // category: '???',
+            id: name, // fullname
+          },
+        );
+        this.item.data.locked = !locked;
+      } catch (err) {
+        console.error(err);
+        this.error = err;
+      } finally {
+        await minWait;
+        this.busy = false;
+      }
     },
   },
 };
 </script>
 
 <style lang="sass">
-.btn-remove
+.btn-lock
   cursor: pointer
 </style>
--- components/NsfwButton.vue
+++ components/NsfwButton.vue
@@ -1,16 +1,15 @@
 <template lang="pug">
-  span.btn-remove(
-    v-show='false /* @todo */'
+  span.btn-nsfw(
     v-disabled="busy"
     :class='classes'
     @click.stop.prevent="nsfw"
   )
     i.fa.fa-fw.fa-btn.fa-spinner.fa-spin(v-if='busy')
     i.fa.fa-fw.fa-btn.fa-exclamation-triangle(v-else)
-    span(v-if='busy && item.data.nsfw') unmarking nsfw
-    span(v-else-if='busy && !item.data.nsfw') marking nsfw
-    span(v-else-if='item.data.nsfw') nsfw
-    span(v-else) nsfw
+    span(v-if='busy && item.data.over_18') unmarking nsfw
+    span(v-else-if='busy && !item.data.over_18') marking nsfw
+    span(v-else-if='item.data.over_18') unmark nsfw
+    span(v-else) mark nsfw
 </template>
 
 <script>
@@ -39,31 +38,31 @@ export default {
   },
   methods: {
     async nsfw($event) {
-      // const { removed, name } = this.item.data;
-      // const minWait = startMinWait();
-      // try {
-      //   this.busy = true;
-      //   const response = await this.$reddit.post(
-      //     `/api/${removed ? 'remove' : 'unremove'}`,
-      //     {
-      //       // category: '???',
-      //       id: name, // fullname
-      //     },
-      //   );
-      //   this.item.data.removed = !removed;
-      // } catch (err) {
-      //   console.error(err);
-      //   this.error = err;
-      // } finally {
-      //   await minWait;
-      //   this.busy = false;
-      // }
+      const { over_18, name } = this.item.data;
+      const minWait = startMinWait();
+      try {
+        this.busy = true;
+        const response = await this.$reddit.post(
+          `/api/${over_18 ? 'unmarknsfw' : 'marknsfw'}`,
+          {
+            // category: '???',
+            id: name, // fullname
+          },
+        );
+        this.item.data.over_18 = !over_18;
+      } catch (err) {
+        console.error(err);
+        this.error = err;
+      } finally {
+        await minWait;
+        this.busy = false;
+      }
     },
   },
 };
 </script>
 
 <style lang="sass">
-.btn-remove
+.btn-nsfw
   cursor: pointer
 </style>
