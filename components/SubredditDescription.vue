3104024d02d7734d1829b5d504227bcc6376a5cf
AnarchonGE <33235178+AnarchonGE@users.noreply.github.com>
2019-08-16 08:37:45 -0400

    Update SubredditDescription.vue
    Remove debug text

--- components/SubredditDescription.vue
+++ components/SubredditDescription.vue
@@ -1,16 +1,9 @@
 <template lang="pug">
   .community-details
     h5.text-center Community Details
-    p
-      .d-block.d-sm-none xs
-      .d-none.d-sm-block.d-md-none sm
-      .d-none.d-md-block.d-lg-none md
-      .d-none.d-lg-block.d-xl-none lg
-      .d-none.d-xl-block xl
     hr
     h4 /r/{{subreddit.data.display_name}}
     ItemHtml(:value='subreddit.data.description_html')
-    p details
     //- pre: tt {{ {subreddit} }}
     //- pre: tt {{ {sidebar} }}
     //- pre: tt {{ {rules} }}
