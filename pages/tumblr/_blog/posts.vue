2aab2c49d751db84b51db8b8ec76ca46cc2c6add
anonymous <>
2020-04-17 19:33:33 +0000

    WIP

--- pages/tumblr/_blog/posts.vue
+++ pages/tumblr/_blog/posts.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
   .container
-    template(v-for="post in posts")
+    h2 posts
+    Loading(v-if="fetching")
+    template(v-else v-for="post in posts")
       TumblrPostForm(v-if="post.id === editingId" :post="post" :key="post.id")
       TumblrPostRow(v-else :post="post" :key="post.id")
 </template>
 
 <script>
-import tumblr from '~/lib/tumblr/client';
 import TumblrPostRow from '~/components/Tumblr/TumblrPostRow';
 
 export default {
