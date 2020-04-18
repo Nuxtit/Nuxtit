318a6ba66e112704e8c7cebcb3d69420de2b605e
anon <>
2020-04-18 00:12:03 +0000

    typo

--- pages/tumblr/_blog/drafts.vue
+++ pages/tumblr/_blog/drafts.vue
@@ -18,7 +18,7 @@ export default {
   mixins: [TumblrItems('blogDrafts')],
   components: {
     TumblrPostRow,
-    TumblePagination,
+    TumblrPagination,
   },
 };
 </script>
--- pages/tumblr/_blog/posts.vue
+++ pages/tumblr/_blog/posts.vue
@@ -18,7 +18,7 @@ export default {
   mixins: [TumblrItems('blogPosts')],
   components: {
     TumblrPostRow,
-    TumblePagination,
+    TumblrPagination,
   },
 };
 </script>
